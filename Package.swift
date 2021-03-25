21c17d66bb2817c04b2d1f2e3094f9220ca53328
Soares e Neves <soareseneves@terra.com.br>
2021-03-25 12:29:13 -0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "DLRadioButton",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS("6.0")
+    ],
+    products: [
+        .library(name: "DLRadioButton", targets: ["DLRadioButton"])
+    ],
+    targets: [
+        .target(
+            name: "DLRadioButton",
+            path: "DLRadioButton",
+            exclude: [
+                "Info.plist",
+            ],
+            resources: [.process("Resources")]
+        )
+    ]
+)
